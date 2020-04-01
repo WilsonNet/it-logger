bf97f75cc7452fcebff180f82adadca0c46baddc
Wilson Neto <wilsonpsn@hotmail.com>
2020-04-01 09:03:59 -0300

    Feito a searchbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import React, { useEffect } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
+import SearchBar from './components/layout/SearchBar';
 
 function App() {
   useEffect(() => {
+    // Init Materialize JS
     M.AutoInit();
   });
+  return (
+    <>
+      <SearchBar />
+      <h1>oi</h1>
+    </>
+  );
 }
 
 export default App;
