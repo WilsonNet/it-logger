42c2cf1290d5fce0424330ea78f572be6789acef
Wilson Neto <wilsonpsn@hotmail.com>
2020-04-08 08:51:49 -0300

    add btn added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
 
 function App() {
   useEffect(() => {
@@ -15,6 +16,7 @@ function App() {
     <>
       <SearchBar />
       <div className="container">
+        <AddBtn />
         <Logs />
       </div>
     </>
