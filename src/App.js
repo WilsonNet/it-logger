dc5d1d78a24a187e2470eac3d75fd2e6d35e33e5
Wilson Neto <wilsonpsn@hotmail.com>
2020-04-22 09:14:50 -0300

    Added tech-modal

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import AddBtn from './components/layout/AddBtn';
 import AddLogModal from './components/logs/AddLogModal';
 import EditLogModal from './components/logs/EditLogModa';
 import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 function App() {
   useEffect(() => {
@@ -24,6 +25,7 @@ function App() {
         <AddTechModal />
         <EditLogModal />
         <Logs />
+        <TechListModal />
       </div>
     </>
   );
